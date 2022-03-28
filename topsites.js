1c9337e65fb3c880525216f8116cf27ac7f30083
Arnaud Bonetti <arnaudbonetti@me.com>
2022-03-28 23:07:18 +0200

    store images locally and add expiry date

--- topsites.js
+++ topsites.js
@@ -1,7 +1,7 @@
-function createSiteView(siteid, site, picurl) {
+function createSiteView(siteid, site, pic) {
   document.getElementById(siteid).innerHTML = `
     <a href="https://${site}">
-      <img class="sitepic" src="${picurl}">
+      <img class="sitepic" src="${pic}">
     </a>
     <br>
     <span class="url">${site.split('/')[0]}</span>
@@ -17,21 +17,38 @@ function createErrorView(siteid, site) {
   `;
 }
 
+function storePic(site, blob) {
+  var reader = new FileReader();
+  reader.readAsDataURL(blob);
+  reader.onloadend = function() {
+    localStorage.setItem(conf[site], reader.result);
+    date = new Date();
+    localStorage.setItem(conf[site] + '_exp', date.setDate(date.getDate() + 5));
+    createSiteView(site, conf[site], localStorage.getItem(conf[site]));
+  }
+}
+
 for(const site in conf) {
+  if(localStorage.getItem(conf[site] + '_exp')) {
+    today = new Date();
+    expiry = new Date(localStorage.getItem(conf[site] + '_exp'));
+    if(today > expiry)
+      localStorage.removeItem(conf[site]);
+  }
+
   if(!localStorage.getItem(conf[site])) { // no cached snapshot for this site
     fetch('https://api.apiflash.com/v1/urltoimage?access_key=' + apiflashKey + '&url=https://' + conf[site]
-      + '&width=' + Math.round(window.innerWidth*1.8) + '&height=' + Math.round(window.innerHeight*1.8) + '&response_type=json')
+      + '&width=' + Math.round(window.innerWidth*1.6) + '&height=' + Math.round(window.innerHeight*1.6))
     .then(function(response) {
       if(response.ok)
-        return response.json();
+        return response.blob();
       else
         return {error_type: "request"};
-    }).then(function(picjson) {
-      if(!('error_type' in picjson) && 'url' in picjson) {
-        createSiteView(site, conf[site], picjson.url);
-        localStorage.setItem(conf[site], picjson.url);
+    }).then(function(picture) {
+      if(!('error_type' in picture)) {
+        storePic(site, picture);
       } else {
-        console.log(picjson);
+        console.log(picture);
         createErrorView(site, conf[site]);
       }
     });
