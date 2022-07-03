71ae1f3c44ebcb375b1f2f3c292e79db3f2c69a2
Arnaud Bonetti <arnaudbonetti@me.com>
2022-07-03 13:48:03 +0200

    API key now stored in local storage

--- topsites.js
+++ topsites.js
@@ -23,7 +23,11 @@ function storePic(site, blob) {
   reader.onloadend = function() {
     localStorage.setItem(conf[site], reader.result);
     date = new Date();
-    localStorage.setItem(conf[site] + '_exp', date.setDate(date.getDate() + 5));
+    if(conf[site] === 'apod.nasa.gov') // daily update for NASA's picture of the day
+      localStorage.setItem(conf[site] + '_exp', date.setDate(date.getDate() + 1));
+    else
+      localStorage.setItem(conf[site] + '_exp', date.setDate(date.getDate() + 10));
+
     createSiteView(site, conf[site], localStorage.getItem(conf[site]));
   }
 }
@@ -31,14 +35,16 @@ function storePic(site, blob) {
 for(const site in conf) {
   if(localStorage.getItem(conf[site] + '_exp')) {
     today = new Date();
-    expiry = new Date(localStorage.getItem(conf[site] + '_exp'));
-    if(today > expiry)
+    expiry = new Date(parseInt(localStorage.getItem(conf[site] + '_exp')));
+    if(today > expiry) {
       localStorage.removeItem(conf[site]);
+      console.log(conf[site]);
+    }
   }
 
   if(!localStorage.getItem(conf[site])) { // no cached snapshot for this site
-    fetch('https://api.apiflash.com/v1/urltoimage?access_key=' + apiflashKey + '&url=https://' + conf[site]
-      + '&width=' + Math.round(window.innerWidth*1.6) + '&height=' + Math.round(window.innerHeight*1.6))
+    fetch('https://api.apiflash.com/v1/urltoimage?access_key=' + localStorage.getItem('apiflashKey') + '&url=https://' + conf[site]
+      + '&width=' + Math.round(window.innerWidth*1.4) + '&height=' + Math.round(window.innerHeight*1.4))
     .then(function(response) {
       if(response.ok)
         return response.blob();
